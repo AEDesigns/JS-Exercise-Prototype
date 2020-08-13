475f3d4056d1f7bca514f04354b8f5eb027b1099
aedevdesigns@gmail.com <aedevdesigns@gmail.com>
2020-08-13 17:29:26 -0500

    Finish task 3

--- index.js
+++ index.js
@@ -73,8 +73,14 @@ Person.prototype.toString=function(){
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+ this.tank = 0;
+ this.odometer = 0;
+ this.mpg = milesPerGallon;
+}
 
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank += gallons;
 }
 
 /*
@@ -84,8 +90,15 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
+}
 
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
